47fdf6f1719fc8f4781dc616d8b6f9dc440b16e6
Grupo-Noctus <singned@outlook.com>
2025-04-11 23:07:11 -0300

    Feat: listar e buscar itens mágicos

--- src/item-magico/erros/itens.erros.ts
+++ src/item-magico/erros/itens.erros.ts
@@ -1,7 +1,13 @@
-import { BadRequestException } from "@nestjs/common";
+import { BadRequestException, NotFoundException } from "@nestjs/common";
 
 export class ItensInvalidosException extends BadRequestException {
-    constructor(ids: number[]) {
-      super(`IDs inválidos: ${ids.join(', ')}`);
-    }
+  constructor(ids: number[]) {
+    super(`IDs inválidos: ${ids.join(', ')}`);
   }
+}
+
+export class ItemMagicoNaoEncontradoException extends NotFoundException {
+  constructor(id: number | string) {
+    super(`Item Mágico com ID '${id}' não encontrado.`);
+  }
+}
--- src/item-magico/item-magico.service.ts
+++ src/item-magico/item-magico.service.ts
@@ -1,8 +1,9 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ItemMagicoResponse } from './dto/item-response.dto';
-import { ItensInvalidosException } from './erros/itens.erros';
+import { ItemMagicoNaoEncontradoException, ItensInvalidosException } from './erros/itens.erros';
 import { ItemMagicoRequest } from './dto/item-request.dto';
+import { ItemEnum } from './enum/item.enum';
 
 @Injectable()
 export class ItemMagicoService {
@@ -44,6 +45,46 @@ export class ItemMagicoService {
             return newItemMagico;
         } catch (erro) {
             throw new InternalServerErrorException('Erro ao cadastrar item mágico');
+        }
+    }
+
+    async buscarItemMagico(idItemMagico: number): Promise<ItemMagicoResponse> {
+        try {
+            const itemMagico = await this.prisma.itemMagico.findUnique({
+                where: { id: idItemMagico},
+            });
+
+            if (!itemMagico) {
+                throw new ItemMagicoNaoEncontradoException(idItemMagico);
+            }
+                
+            return {
+                id: itemMagico.id,
+                nome: itemMagico.nome,
+                item: itemMagico.item,
+                forca: itemMagico.forca,
+                defesa: itemMagico.defesa,
+            };
+        } catch (erro) {
+            if(erro instanceof ItemMagicoNaoEncontradoException) throw erro;
+            throw new InternalServerErrorException();
+        }
+    }
+
+    async listarItensMagicos (): Promise<ItemMagicoResponse[]>{
+        try {
+            const itensMagicos = await this.prisma.itemMagico.findMany();
+
+            return itensMagicos.map((item) => ({
+                id: item.id,
+                nome: item.nome,
+                item: item.item as ItemEnum,
+                forca: item.forca,
+                defesa: item.defesa,
+              }));
+        } catch (erro) {
+            console.error('Erro inesperado ao buscar os itens mágicos:', erro);
+            throw new InternalServerErrorException();
         }
     }
 }
